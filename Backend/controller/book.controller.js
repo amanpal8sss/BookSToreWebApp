136b758fc6cfb8031065ce8c11eb0c6ffb15edb4
AMAN PAL <amanpal8sss76@gmail.com>
2024-06-11 13:09:56 +0530

    updateBook functionality

--- Backend/controller/book.controller.js
+++ Backend/controller/book.controller.js
@@ -25,3 +25,18 @@ export const addBook = async(req,res)=>{
     
   }
 }
+
+export const updateBook = async(req,res)=>{
+  try {
+    const {Bookid} = req.params;
+    await Book.findByIdAndUpdate(Bookid,
+      req.body
+    );
+    
+    
+    res.status(200).json({message:"Book Updated Successfully!"});
+  } catch (error) {
+    res.status(500).json({message:error.message});
+    
+  }
+}
--- Backend/routes/book.route.js
+++ Backend/routes/book.route.js
@@ -3,12 +3,16 @@ import express from 'express';
 import Book from "../models/book.model.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
-import {addBook} from '../controller/book.controller.js';
+import {addBook,updateBook} from '../controller/book.controller.js';
 import {authenticateToken,isAdmin} from '../routes/userAuth.js'
 const router = express.Router();
 
 //add-book --admin
-
 router.post('/addBook',authenticateToken,isAdmin,addBook);
+
+//update-book --admin
+router.put('/updateBook/:Bookid',authenticateToken,isAdmin,updateBook);
+
+
 
 export default router;
